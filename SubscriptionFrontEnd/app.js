28c142be7f8afc9309ec633e4fbffdf489e2dcbf
Ikey Benzaken <ikey.benz@gmail.com>
2018-03-18 16:49:19 -0400

    New Form Look

--- SubscriptionFrontEnd/app.js
+++ SubscriptionFrontEnd/app.js
@@ -10,25 +10,49 @@
  firebase.initializeApp(config);
  var database = firebase.database();
 
- function submitPreferences() {
-     const emailAddress = document.getElementById('email').value;
-     const subscriptions = document.getElementById('subs').value;
-     const zip = document.getElementById('zip').value;
-     database.ref.child("Subscribers").orderByChild("Email").equalTo(emailAddress).once("value",snapshot => {
-         const userData = snapshot.val();
-         if (userData) {
-             alert("The email address entered is already registered for subscriptions.");
-         } else {
-             database.ref('Subscribers').push({
-                 Email: emailAddress,
-                 Subscriptions: subscriptions,
-                 ZipCode: zip
-             })
-             database.ref('Zipcodes/' + zip).push(emailAddress);
+function submitPreferences() {
+    const emailAddress = document.getElementById('email').value;
+    const subscriptions = document.getElementById('subs').value;
+    const zip = document.getElementById('zip').value;
+    database.ref("Subscribers").orderByChild("Email").equalTo(emailAddress).once("value",snapshot => {
+        const userData = snapshot.val();
+        if (userData) {
+            alert("The email address entered is already registered for subscriptions.");
+        } else {
+            database.ref('Subscribers').push({
+                Email: emailAddress,
+                Subscriptions: subscriptions,
+                ZipCode: zip
+            })
+            database.ref('Zipcodes/' + zip).push(emailAddress);
+            alert("Your subscription preferences have been saved.\nThank you!");
+        }
+    });
+}
 
-             alert("Your subscription preferences have been saved.\nThank you!");
-         }
-});
+function toggleForm() {
+     $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+}
+function requiredFieldsAreFilledOut() {
+    if (document.getElementById('email').value == "") {
+        return false
+    }
+    const subs = [1,2,3,4];
+    var atLeastOnecheckboxIsSelected = false;
+    for (i in subs) {
+        if (document.getElementById(`subs-${i}`).checked == true) {
+            atLeastOnecheckboxIsSelected = true;
+        }
+    }
+    return atLeastOnecheckboxIsSelected
+}
+function subscribe() {
+    if (requiredFieldsAreFilledOut() && document.getElementById('zipcode').value != "") {
+        
+    }
+}
+function editSubscriptions() {
+    if (requiredFieldsAreFilledOut()) {
 
-
- }
+    }
+}
