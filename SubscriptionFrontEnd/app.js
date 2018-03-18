6ad2c548acc4a3c8dcc297d35dbc0aef1deaea4c
Ikey Benzaken <ikey.benz@gmail.com>
2018-03-18 16:48:38 -0400

    Checks if email is already registered

--- SubscriptionFrontEnd/app.js
+++ SubscriptionFrontEnd/app.js
@@ -12,19 +12,31 @@
 
  function submitPreferences() {
      const emailAddress = document.getElementById('email').value;
+     const password = document.getElementById('password').value;
      const subscriptions = document.getElementById('subs').value;
      const zip = document.getElementById('zip').value;
-     database.ref.child("Subscribers").orderByChild("Email").equalTo(emailAddress).once("value",snapshot => {
+     // Check if email is not already registered
+
+     // If it is, check if the password entered matches the one stored with said email
+
+     // If password is no good, alert the user that the password is incorrect
+
+     // Make a reset password screen (oy that's going to be a pain)
+
+
+     database.ref().child("Subscribers").orderByChild("Email").equalTo(emailAddress).once("value",snapshot => {
          const userData = snapshot.val();
          if (userData) {
              alert("The email address entered is already registered for subscriptions.");
          } else {
-             database.ref('Subscribers').push({
-                 Email: emailAddress,
-                 Subscriptions: subscriptions,
-                 ZipCode: zip
-             })
-             database.ref('Zipcodes/' + zip).push(emailAddress);
+
+             // database.ref('Subscribers').push({
+             //     Email: emailAddress,
+             //     Password: password;
+             //     Subscriptions: subscriptions,
+             //     ZipCode: zip
+             // });
+             // database.ref('Zipcodes/' + zip).push(emailAddress);
 
              alert("Your subscription preferences have been saved.\nThank you!");
          }
